678f3a97305dfec0f3b008acf2087cd636f35266
VladZg <vlad.v.zag@gmail.com>
2022-10-06 22:51:09 +0300

    На ветке main Ваша ветка обновлена в соответствии с «origin/main». Изменения, которые будут включены в коммит: 	изменено:      Constants.h 	изменено:      DumpGraphics.h 	изменено:      Stack/Consts.h 	изменено:      Stack/Log.cpp 	изменено:      asm.cpp 	изменено:      cpu.cpp 	изменено:      disasm.cpp 	изменено:      main.cpp 	изменено:      make.cpp
     Неотслеживаемые файлы:
    	FilesInfo.txt
    	Log.txt
    	Source_default.txt
    	Source_output.asm
    	Source_output_disasmed.txt
    	asm
    	cpu
    	disasm
    	main
    	make

--- Constants.h
+++ Constants.h
@@ -2,20 +2,23 @@
 #define CONSTANTS_H
 
 enum {
-      CMD_HLT     = 0,
-      CMD_PUSH    = 1,
-      CMD_ADD     = 2,
-      CMD_SUB     = 3,
-      CMD_MUL     = 4,
-      CMD_DIV     = 5,
-      CMD_OUT     = 6,
-      CMD_DUMP    = 7,
-      CMD_PIN     = 8,
+      CMD_HLT          = 0,
+      CMD_PUSH         = 1,
+      CMD_ADD          = 2,
+      CMD_SUB          = 3,
+      CMD_MUL          = 4,
+      CMD_DIV          = 5,
+      CMD_OUT          = 6,
+      CMD_DUMP         = 7,
+      CMD_PIN          = 8,
 
-      CP_FILECODE = 1337,
-      CMD_VERSION = 2,
+      WRNG_CMD         = -1,
+      WRNG_CMD_VERSION = -1,
 
-      TECH_INFO_SIZE = 3
+      CP_FILECODE      = 1337,
+      CMD_VERSION      = 4,
+
+      TECH_INFO_SIZE   = 3
      };
 
 #endif
