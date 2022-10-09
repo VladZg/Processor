5fdefca71c4223da761003991cfc7a8a19cb4def
VladZg <vlad.v.zag@gmail.com>
2022-10-10 02:25:15 +0300

    новый файл:    CpuGraphics.h

---
+++ CpuGraphics.h
@@ -0,0 +1,49 @@
+#ifndef DUMPGRAPHICS_H
+#define DUMPGRAPHICS_H
+
+#include <stdio.h>
+#include <cstring>
+#include "Stack/ColourConsts.h"
+#include "Stack/Stack.h"
+
+#ifndef  NDUMP
+
+#define SimpleStackDump( stack_ptr )                 \
+{                                                    \
+    SimpleStackDump_(stack_ptr);                     \
+    fprintf(stderr, "\n");                           \
+}
+
+#else
+
+#define SimpleStackDump( stack_ptr ) {}
+
+#endif
+
+void WriteNSymb(size_t n, char symb)
+{
+    for (int i = 0; i < n; i++)
+        fprintf(stderr, "%c", symb);
+}
+
+int PrintRusFlag()
+{
+    fprintf(stderr, "\n");
+    fprintf(stderr, KWHT "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n" KNRM);
+    fprintf(stderr, KBLU "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n" KNRM);
+    fprintf(stderr, KRED "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n"
+                         "  RUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIARUSSIA\n" KNRM);
+    fprintf(stderr, "\n");
+
+    return 0xDED;
+}
+
+#endif
